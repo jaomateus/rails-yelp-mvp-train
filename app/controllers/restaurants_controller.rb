fa4d78f848c01a8772ef1efe37c1e8706899256e
Joao Mateus <jaomateus@gmail.com>
2023-11-04 21:17:31 +0100

    complete CRUD aactions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,32 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurants, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_restaurants
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_numer, :category)
+  end
 end
